d7ad5bb6cb8bf289f61b73c537d7bd42915ae5d8
suconghou <1126045770@qq.com>
2017-03-26 20:31:50 +0800

    debuglog not upper

--- fastload/fastlog.go
+++ fastload/fastlog.go
@@ -6,23 +6,23 @@ import (
 	"os"
 )
 
-var DebugLog bool = false
+var debuglog bool = false
 
 func debug(args ...interface{}) {
-	if DebugLog {
+	if debuglog {
 		log.Println(args...)
 	}
 }
 
 func halt(args ...interface{}) {
-	if DebugLog {
+	if debuglog {
 		log.Println(args...)
 	}
 	os.Exit(1)
 }
 
 func SetDebug(debug bool) {
-	DebugLog = debug
+	debuglog = debug
 }
 
 func SetOutput(w io.Writer) {
