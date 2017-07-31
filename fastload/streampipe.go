ab0baa99f2bffd92ac1250f215d0cb1c176c07a5
suconghou <1126045770@qq.com>
2017-07-31 23:28:59 +0800

    fix bug

--- fastload/streampipe.go
+++ fastload/streampipe.go
@@ -9,11 +9,11 @@ import (
 // Pipe get resp from url and response the the request
 func Pipe(w http.ResponseWriter, r *http.Request, url string, rewriteHeader func(http.Header, *http.Response) int) (int64, error) {
 	resp, err := newClient(url, r.Method, r.Header, nil, 3600, nil)
-	defer resp.Body.Close()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("%s", err), 500)
 		return 0, err
 	}
+	defer resp.Body.Close()
 	out := w.Header()
 	for key, value := range resp.Header {
 		out.Add(key, value[0])
