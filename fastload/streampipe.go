d9367910909d0e857a8a245580804a2b3191413e
suconghou <1126045770@qq.com>
2017-07-31 23:16:42 +0800

    fix write header bug

--- fastload/streampipe.go
+++ fastload/streampipe.go
@@ -7,19 +7,20 @@ import (
 )
 
 // Pipe get resp from url and response the the request
-func Pipe(w http.ResponseWriter, r *http.Request, url string, rewriteHeader func(http.ResponseWriter, http.Header)) (int64, error) {
+func Pipe(w http.ResponseWriter, r *http.Request, url string, rewriteHeader func(http.Header, *http.Response) int) (int64, error) {
 	resp, err := newClient(url, r.Method, r.Header, nil, 3600, nil)
+	defer resp.Body.Close()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("%s", err), 500)
 		return 0, err
 	}
+	out := w.Header()
 	for key, value := range resp.Header {
-		w.Header().Add(key, value[0])
+		out.Add(key, value[0])
+	}
+	if rewriteHeader != nil {
+		resp.StatusCode = rewriteHeader(out, resp)
 	}
 	w.WriteHeader(resp.StatusCode)
-	if rewriteHeader != nil {
-		rewriteHeader(w, resp.Header)
-	}
-	defer resp.Body.Close()
 	return io.Copy(w, resp.Body)
 }
