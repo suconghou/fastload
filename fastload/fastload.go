442b5c70aae360fb664c9a71b50de6da250731fb
suconghou <1126045770@qq.com>
2017-08-12 13:52:34 +0800

    add feature FastPipe use fastload

--- fastload/fastload.go
+++ fastload/fastload.go
@@ -185,7 +185,7 @@ func respEnd(resp *http.Response) bool {
 }
 
 //Get load with certain ua and thread thunk
-func Get(url string, start int64, end int64, progress func(received int64, readed int64, total int64, duration float64, start int64, end int64), out io.Writer) (io.ReadCloser, int64, int64, int32, error) {
+func Get(url string, start int64, end int64, progress func(received int64, readed int64, total int64, duration float64, start int64, end int64), out io.Writer) (io.ReadCloser, *http.Response, int64, int64, int32, error) {
 	reqHeader := http.Header{}
 	reqHeader.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
 	return NewLoader(url, 4, 524288, reqHeader, progress, nil, out).Load(start, end, nil)
@@ -211,12 +211,12 @@ func NewLoader(url string, thread int32, thunk int64, reqHeader http.Header, pro
 }
 
 //Load return reader , resp , rangesize , total , thread , error
-func (f *Fastloader) Load(start int64, end int64, mirrors []string) (io.ReadCloser, int64, int64, int32, error) {
+func (f *Fastloader) Load(start int64, end int64, mirrors []string) (io.ReadCloser, *http.Response, int64, int64, int32, error) {
 	start, end = f.fixstartend(start, end)
 	f.startTime = time.Now()
 	resp, url, err := f.loadItem(start, end)
 	if err != nil {
-		return nil, 0, 0, 0, err
+		return nil, nil, 0, 0, 0, err
 	}
 	if resp.ContentLength > 0 {
 		f.total = resp.ContentLength
@@ -292,9 +292,9 @@ func (f *Fastloader) Load(start int64, end int64, mirrors []string) (io.ReadClos
 				}
 			}
 		}()
-		return f, f.total, f.filesize, f.thread, nil
+		return f, resp, f.total, f.filesize, f.thread, nil
 	}
-	return &writeCounter{instance: f, origin: resp.Body}, resp.ContentLength, resp.ContentLength, 1, nil
+	return &writeCounter{instance: f, origin: resp.Body}, resp, resp.ContentLength, resp.ContentLength, 1, nil
 }
 
 func (f *Fastloader) fixstartend(start int64, end int64) (int64, int64) {
--- fastload/streampipe.go
+++ fastload/streampipe.go
@@ -24,3 +24,21 @@ func Pipe(w http.ResponseWriter, r *http.Request, url string, rewriteHeader func
 	w.WriteHeader(resp.StatusCode)
 	return io.Copy(w, resp.Body)
 }
+
+// FastPipe use fastload for pipe, thread should be 2-8 , thunk should be 262144-1048576 (256KB-1024KB), len(mirrors) should no more than thread+1 or nil
+func FastPipe(w http.ResponseWriter, r *http.Request, url string, thread int32, thunk int64, mirrors []string, rewriteHeader func(http.Header, *http.Response) int, transport *http.Transport) (int64, error) {
+	body, resp, _, _, _, err := NewLoader(url, thread, thunk, r.Header, nil, transport, nil).Load(0, 0, mirrors)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), 500)
+		return 0, err
+	}
+	out := w.Header()
+	for key, value := range resp.Header {
+		out.Add(key, value[0])
+	}
+	if rewriteHeader != nil {
+		resp.StatusCode = rewriteHeader(out, resp)
+	}
+	w.WriteHeader(resp.StatusCode)
+	return io.Copy(w, body)
+}
