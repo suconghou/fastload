f42ec0fea7a7e5692b5bf93fad769f847e1da09b
suconghou <1126045770@qq.com>
2017-08-16 11:25:16 +0800

    http proxy

--- fastload/streampipe.go
+++ fastload/streampipe.go
@@ -3,14 +3,17 @@ package fastload
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 // Pipe get resp from url and response the the request can also be a http proxy
 func Pipe(w http.ResponseWriter, r *http.Request, url string, rewriteHeader func(http.Header, *http.Response) int, timeout int64, transport *http.Transport) (int64, error) {
 	resp, err := NewClient(url, r.Method, r.Header, nil, timeout, r.Body, transport)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("%s", err), 500)
+		http.Error(w, err.Error(), 500)
 		return 0, err
 	}
 	defer resp.Body.Close()
@@ -29,7 +32,7 @@ func Pipe(w http.ResponseWriter, r *http.Request, url string, rewriteHeader func
 func FastPipe(w http.ResponseWriter, r *http.Request, url string, thread int32, thunk int64, mirrors []string, rewriteHeader func(http.Header, *http.Response) int, transport *http.Transport) (int64, error) {
 	body, resp, _, _, _, err := NewLoader(url, thread, thunk, r.Header, nil, transport, nil).Load(0, 0, mirrors)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("%s", err), 500)
+		http.Error(w, err.Error(), 500)
 		return 0, err
 	}
 	out := w.Header()
@@ -41,4 +44,38 @@ func FastPipe(w http.ResponseWriter, r *http.Request, url string, thread int32, 
 	}
 	w.WriteHeader(resp.StatusCode)
 	return io.Copy(w, body)
+}
+
+// HTTPProxy is http only proxy no https
+func HTTPProxy(w http.ResponseWriter, r *http.Request) error {
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		errorMsg := "your ResponseWriter does not support Hijacking! "
+		http.Error(w, errorMsg, http.StatusInternalServerError)
+		return fmt.Errorf(errorMsg)
+	}
+	_, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	hostPortURL, err := url.Parse(r.RequestURI)
+	address := hostPortURL.Host
+	if strings.Index(address, ":") == -1 {
+		address = address + ":80"
+	}
+	server, err := net.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+	r.Write(server)
+	p1die := make(chan struct{})
+	go func() { io.Copy(server, bufrw); close(p1die) }()
+	p2die := make(chan struct{})
+	go func() { io.Copy(bufrw, server); close(p2die) }()
+	select {
+	case <-p1die:
+	case <-p2die:
+	}
+	return nil
 }
